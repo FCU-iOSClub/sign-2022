e33fc17ab99e0a53c536b379cb66fce3f193105a
TonyPepe <tonybear55665566@gmail.com>
2022-11-11 15:34:39 +0800

    init commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,10 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1 class="text-10">iOS Club</h1>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
