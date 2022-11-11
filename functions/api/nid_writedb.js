3ef00dfe15d3df870e51a1bc30079b4fa22adf25
TonyPepe <tonybear55665566@gmail.com>
2022-11-12 01:54:30 +0800

    delete sission

--- functions/api/nid_writedb.js
+++ functions/api/nid_writedb.js
@@ -1,72 +1,70 @@
 import { parse } from 'cookie'
 
 export async function onRequestPost({ env, request }) {
-  try {
-    /*
+  /*
     formData: {
       uid,
       accessToken,
     }
   */
-    const reqBody = await request.formData()
+  const reqBody = await request.formData()
 
-    // get nid data from KV by session in cookie
-    const cookie = parse(request.headers.get('Cookie') || '')
-    const nid_data = await env.NID_SISSION.get(cookie['NID_SISSION'], {
-      type: 'json',
-    })
-    // convert nid_data to firebase document
-    const document = {
-      fields: {
-        id: {
-          stringValue: nid_data.id,
-        },
-        name: {
-          stringValue: nid_data.name,
-        },
-        classname: {
-          stringValue: nid_data.classname,
-        },
-        unit_id: {
-          stringValue: nid_data.unit_id,
-        },
-        unit_name: {
-          stringValue: nid_data.unit_name,
-        },
-        dept_id: {
-          stringValue: nid_data.dept_id,
-        },
-        dept_name: {
-          stringValue: nid_data.dept_name,
-        },
-        Email: {
-          stringValue: nid_data.Email,
-        },
+  // get nid data from KV by session in cookie
+  const cookie = parse(request.headers.get('Cookie') || '')
+  const nid_data = await env.NID_SISSION.get(cookie['NID_SISSION'], {
+    type: 'json',
+  })
+  // delete session from KV
+  env.NID_SISSION.delete(cookie['NID_SISSION'])
+  // convert nid_data to firebase document
+  const document = {
+    fields: {
+      id: {
+        stringValue: nid_data.id,
       },
-    }
-
-    // create firebase firestore post options
-    const options = {
-      method: 'POST',
-      headers: {
-        Authorization: 'Bearer ' + reqBody.get('accessToken'),
-        'content-type': 'application/json',
+      name: {
+        stringValue: nid_data.name,
       },
-      body: JSON.stringify(document),
-    }
-    // send post to firebase, create user nid
-    await fetch(
-      'https://firestore.googleapis.com/v1/projects/singin-8db60/databases/(default)/documents/user-nid?documentId=' +
-        reqBody.get('uid'),
-      options
-    )
-
-    // redirect user to root '/'
-    const response = new Response('', { status: 303 })
-    response.headers.append('Location', '/')
-    response.headers.append('Set-Cookie', `NID_SISSION=`)
-    return response
-  } catch (e) {
-    return new Response(e.stack)
+      classname: {
+        stringValue: nid_data.classname,
+      },
+      unit_id: {
+        stringValue: nid_data.unit_id,
+      },
+      unit_name: {
+        stringValue: nid_data.unit_name,
+      },
+      dept_id: {
+        stringValue: nid_data.dept_id,
+      },
+      dept_name: {
+        stringValue: nid_data.dept_name,
+      },
+      Email: {
+        stringValue: nid_data.Email,
+      },
+    },
   }
+
+  // create firebase firestore post options
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + reqBody.get('accessToken'),
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(document),
+  }
+  // send post to firebase, create user nid
+  await fetch(
+    'https://firestore.googleapis.com/v1/projects/singin-8db60/databases/(default)/documents/user-nid?documentId=' +
+      reqBody.get('uid'),
+    options
+  )
+
+  // redirect user to root '/'
+  const response = new Response('', { status: 303 })
+  response.headers.append('Location', '/')
+  response.headers.append('Set-Cookie', `NID_SISSION=`)
+  return response
 }
