f268b05a772e3df18c71513338c954eb1193debb
TonyPepe <tonybear55665566@gmail.com>
2022-11-12 03:43:18 +0800

    format

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <!-- Please remove this file from your project -->
 <template>
   <div>
-    <nav class="relative w-screen bg-gray-100 shadow-lg h-fit px-3 py-1 z-10">
-      <div class="flex justify-between">
+    <nav class="relative w-screen bg-gray-100 shadow-lg h-fit">
+      <div class="flex justify-between p-2">
         <NuxtLink to="/" class="text-8 no-underline text-black"
           >iOS Club</NuxtLink
         >
         <span
           v-if="loginStatus == 0"
           @click="login"
-          class="w-fit my-3 mx-6 rounded-full break-normal bg-blue-300 cursor-pointer"
+          class="w-fit py-3 px-6 rounded-full break-normal bg-blue-300 cursor-pointer"
         >
-          登入
+          Google 登入
         </span>
         <span
           v-if="loginStatus == 1"
@@ -23,15 +23,18 @@
       </div>
     </nav>
 
-    <div class="h-10" />
-    <h1 class="text-center">簽到系統</h1>
-    <a
-      v-if="loginStatus == 1"
-      class="shadow-md w-fit py-3 px-6 rounded-full break-normal bg-blue-300 cursor-pointer text-black no-underline"
-      href="https://opendata.fcu.edu.tw/fcuOauth/Auth.aspx?client_id=638037929185.f4b4a8ed044a488c85074c7f9e5ada47.sign.iosclub.tw&client_url=https://sign.iosclub.tw/api/nid_callback"
-    >
-      Bind Nid
-    </a>
+    <main class="mx-2 md:mx-10 lg:mx-20">
+      <h1 class="text-left">簽到系統</h1>
+      <a
+        v-if="loginStatus == 1"
+        class="shadow-md w-fit py-3 px-6 rounded-full break-normal bg-blue-300 cursor-pointer text-black no-underline"
+        href="https://opendata.fcu.edu.tw/fcuOauth/Auth.aspx?client_id=638037929185.f4b4a8ed044a488c85074c7f9e5ada47.sign.iosclub.tw&client_url=https://sign.iosclub.tw/api/nid_callback"
+      >
+        Bind Nid
+      </a>
+      <!-- message -->
+      <p v-if="loginStatus == 0">請先登入</p>
+    </main>
   </div>
 </template>
 
